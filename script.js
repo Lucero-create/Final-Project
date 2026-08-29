d81c8c40983a4d1da3fa84cff35ebeb60886cfb4
lucy medrano <lucymedrano@MacBookAir.lan>
2026-08-29 11:36:23 -0700

    second submit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  menu.classList.toggle('active');
+function openMenu() {
+  document.querySelector(".menu").classList.add("menu--open");
+}
+function closeMenu() {
+  document.querySelector(".menu").classList.remove("menu--open");
 }
